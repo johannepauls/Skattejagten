42ddeb35dbe9cbc3139db7afc5497294711b8d94
Johanne Kristine Kappel Pauls <johannekristinekappelpauls@johannes-mbp.eduroam.wireless.dtu.dk>
2018-06-08 11:48:54 +0200

    Initial Commit

---
+++ Skattejagten/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Skattejagten
+//
+//  Created by Johanne Kristine Kappel Pauls on 08/06/2018.
+//  Copyright © 2018 Johanne Kristine Kappel Pauls. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
